49fa845c92ac7aa7112fedd9fdad61e19a50efda
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2020-03-12 19:18:30 -0400

    Update test_ops.py

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -4,4 +4,4 @@ from project import ops
 def test_add_fun():
     assert ops.add_fun(1, 2) == 3
     with pytest.raises(AssertionError, match 'wrong type!')
-        ops.add_fun(1, '2')
+        ops.add_fun('1', 2)
