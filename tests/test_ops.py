df24c3f9002929aae31149d59831039befe66833
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2020-03-12 19:28:11 -0400

    Update test_ops.py

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -2,6 +2,6 @@ import pytest
 from project import ops
 
 def test_add_fun():
-    assert ops.add_fun(1, 2) == 3
-    with pytest.raises(AssertionError, match='wrong type!')
-        ops.add_fun('1', 2)
+  assert ops.add_fun(1,2) == 3
+  with pytest.raises(AssertionError,match='wrong type!'):
+    ops.add_fun('1',2)
